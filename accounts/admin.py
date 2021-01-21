c4aa4b0d9e6553bf226d2a3d43608d485c7f3047
QuiD-0 <wodnd101@naver.com>
2021-01-21 08:25:24 +0000

    accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'user']
+    lsit_display_links = ['nickname', 'user']
+    search_field = ['nickname']
