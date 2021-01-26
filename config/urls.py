4505e0f97db4f150cbf2564845bb403c28abb151
QuiD-0 <wodnd101@naver.com>
2021-01-26 05:27:57 +0000

    post_new, js

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 
 urlpatterns = [
@@ -25,7 +26,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     
     path('post/',include('post.urls',namespace='post')),
-    path('',lambda r:redirect('post:post_list'),name='root')
+    path('',lambda r: redirect('post:post_list'),name='root')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
