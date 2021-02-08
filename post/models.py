fa3fd8d670357a8e0437b799daaeeb840d04c578
QuiD-0 <wodnd101@naver.com>
2021-02-08 08:36:05 +0000

    tag

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+import re
 
 def photo_path(instance,filename):
     from time import strftime
@@ -13,22 +14,41 @@ def photo_path(instance,filename):
     return '{}/{}/{}.{}'.format(strftime('post/%y/%m/%d'), instance.author.username,pid,extension)
     
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = ProcessedImageField(upload_to=photo_path,
-                               processors=[ResizeToFill(600,600)],
-                               format='JPEG',
-                               options={'qualty':90})
-    content =models.CharField(max_length=140, help_text="최대길이 140자 입력이 가능합니다.")
-    
-    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,related_name='like_user_set',through = 'Like')
-    bookmark_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,related_name='bookmark_user_set',through = 'Bookmark')
-    
+                                processors=[ResizeToFill(600, 600)],
+                                format='JPEG',
+                                options={'quality': 90})
+    content = models.CharField(max_length=140, help_text="최대 140자 입력 가능")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                           blank=True,
+                                           related_name='like_user_set',
+                                           through='Like')  # post.like_set 으로 접근 가능
+    bookmark_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                           blank=True,
+                                           related_name='bookmark_user_set',
+                                           through='Bookmark')  # post.like_set 으로 접근 가능
     
+    
+    
+
     class Meta:
         ordering = ['-created_at']
-        
+
+    # NOTE: content에서 tags를 추출하여, Tag 객체 가져오기, 신규 태그는 Tag instance 생성, 본인의 tag_set에 등록,
+    def tag_save(self):
+        tags = re.findall(r'#(\w+)\b', self.content)
+
+        if not tags:
+            return
+
+        for t in tags:
+            tag, tag_created = Tag.objects.get_or_create(name=t)
+            self.tag_set.add(tag)  # NOTE: ManyToManyField 에 인스턴스 추가
+
     @property
     def like_count(self):
         return self.like_user_set.count()
@@ -36,9 +56,19 @@ class Post(models.Model):
     @property
     def bookmark_count(self):
         return self.bookmark_user_set.count()
-        
+    
+    
+    
+
     def __str__(self):
         return self.content
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.name
     
 class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
--- post/views.py
+++ post/views.py
@@ -1,48 +1,73 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth import get_user_model
-from .models import Post,Like,Comment
+from .models import Post,Like,Comment,Tag
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from .form import PostForm , CommentForm
 from django.contrib import messages
 import json
 from django.http import HttpResponse
-from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 
 
 # Create your views here.
-def post_list(request):
-    post_list=Post.objects.all()
-    paginator = Paginator(post_list,3)
+def post_list(request, tag=None):
+    tag_all = Tag.objects.annotate(num_post=Count('post')).order_by('-num_post')
+    
+    if tag:
+        post_list = Post.objects.filter(tag_set__name__iexact=tag) \
+            .prefetch_related('tag_set', 'like_user_set__profile', 'comment_set__author__profile',
+                              'author__profile__follower_user', 'author__profile__follower_user__from_user') \
+            .select_related('author__profile')
+    else:
+        post_list = Post.objects.all() \
+            .prefetch_related('tag_set', 'like_user_set__profile', 'comment_set__author__profile',
+                              'author__profile__follower_user', 'author__profile__follower_user__from_user') \
+            .select_related('author__profile')
+    
+    paginator = Paginator(post_list, 3)
     page_num = request.POST.get('page')
+    
     try:
         posts = paginator.page(page_num)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
-         posts = paginator.page(paginator.num_pages)
-            
+        posts = paginator.page(paginator.num_pages)
+        
     if request.is_ajax():
-        return render(request,'post/post_list_ajax.html',{
-            'posts':posts,
+        return render(request, 'post/post_list_ajax.html', {
+            'posts': posts,
         })
-
+    
+    if request.method == 'POST':
+        tag = request.POST.get('tag')
+        tag_clean = ''.join(e for e in tag if e.isalnum())
+        return redirect('post:post_search', tag_clean)
+    
+    
+    
+    
     if request.user.is_authenticated:
-        username=request.user
-        user=get_object_or_404(get_user_model(),username=username)
-        user_profile=user.profile
+        username = request.user
+        user = get_object_or_404(get_user_model(), username=username)
+        user_profile = user.profile
+        follow_set = request.user.profile.get_following
+        follow_post_list = Post.objects.filter(author__profile__in=follow_set)
         
-        following_set= request.user.profile.get_following
-        follow_post_list=Post.objects.filter(author__profile__in=following_set)
-        
-        return render(request,'post/post_list.html',{
-            'user_profile':user_profile,
-            'posts':posts,
-            'follow_post_list':follow_post_list,
+        return render(request, 'post/post_list.html', {
+            'user_profile': user_profile,
+            'tag': tag,
+            'posts': posts,
+            'follow_post_list': follow_post_list,
+            'tag_all': tag_all,
         })
     else:
-        return render(request,'post/post_list.html',{
-            'posts':posts,
+        return render(request, 'post/post_list.html', {
+            'tag': tag,
+            'posts': posts,
+            'tag_all': tag_all,
         })
     
 @login_required
@@ -53,7 +78,7 @@ def post_new(request):
             post=form.save(commit=False)
             post.author = request.user
             post.save()
-            #post.tag_save()
+            post.tag_save()
             messages.info(request,'새 글이 등록되었습니다.')
             return redirect('post:post_list')
     else:
@@ -72,8 +97,8 @@ def post_edit(request,pk):
         form=PostForm(request.POST, request.FILES,instance=post)
         if form.is_valid():
             post = form.save()
-            #post.tag_set.clear()
-            #post.tag_save()
+            post.tag_set.clear()
+            post.tag_save()
             messages.success(request, '수정완료')
             return redirect('post:post_list')
     else:
