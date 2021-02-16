b342286e1f5df5fede213daf4c4c9ac05238067f
QuiD <wodnd101@naver.com>
2021-02-16 22:10:36 +0900

    update

--- accounts/models.py
+++ accounts/models.py
@@ -14,13 +14,15 @@ def user_path(instance,filename):
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nickname = models.CharField('별명',max_length=20, unique=True)
+    
     follow_set = models.ManyToManyField('self',
                                        blank=True,
                                        through = 'follow',
                                        symmetrical=False,)
     
     
-    picture = models.ImageField(upload_to=user_path,
+    picture = ProcessedImageField(upload_to=user_path,
+                                  processors=[ResizeToFill(150,150)],
                                   format='JPEG',
                                   options={'quality':90},
                                   blank=True,
